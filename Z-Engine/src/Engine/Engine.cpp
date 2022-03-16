7a2673073f63658e5d1cd6990ffc5ce8a4e5a829
zzWeeZz <42831999+zzWeeZz@users.noreply.github.com>
2022-03-16 21:52:53 +0100

    dada

--- Z-Engine/src/Engine/Engine.cpp
+++ Z-Engine/src/Engine/Engine.cpp
@@ -1,4 +1,5 @@
 #include "Engine.h"
+#include "d3d11shader.h"
 
 
 void Engine::InitD3D(HWND aHWND)
@@ -33,10 +34,10 @@ void Engine::InitD3D(HWND aHWND)
 	ID3D11Texture2D* backBufferPtr;
 	mySwapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<LPVOID*>(&backBufferPtr));
 
-	myDev->CreateRenderTargetView(backBufferPtr, NULL, &myBackBuffer);
+	myDev->CreateRenderTargetView(backBufferPtr, NULL, &myRenderTargetView);
 	backBufferPtr->Release();
 
-	myDevcon->OMSetRenderTargets(1, &myBackBuffer, NULL);
+	myDevcon->OMSetRenderTargets(1, &myRenderTargetView, NULL);
 	// ----
 
 	D3D11_VIEWPORT viewport;
@@ -45,19 +46,26 @@ void Engine::InitD3D(HWND aHWND)
 	viewport.TopLeftY = 0;
 	viewport.Width = 800;
 	viewport.Height = 600;
-
 	myDevcon->RSSetViewports(1, &viewport);
+
+
+
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
+	};
+	UINT numberOfElements = ARRAYSIZE(layout);
 }
 
 void Engine::InitPipeline()
 {
-	ID3D10Blob* VS, * PS;
 }
 
 void Engine::RenderFrame()
 {
 	float color[5] = { 0,0.2f, 0.5f, 1.f };
-	myDevcon->ClearRenderTargetView(myBackBuffer, color);
+	myDevcon->ClearRenderTargetView(myRenderTargetView, color);
 
 	mySwapchain->Present(0, 0);
 }
@@ -65,7 +73,13 @@ void Engine::RenderFrame()
 void Engine::CleanD3D()
 {
 	mySwapchain->Release();
+	myVertLayout->Release();
+	myVS->Release();
+	myPS->Release();
+	myPS_Buffer->Release();
+	myVS_Buffer->Release();
+	myTriangleVertBuffer->Release();
 	myDev->Release();
-	myBackBuffer->Release();
+	myRenderTargetView->Release();
 	myDevcon->Release();
 }
--- Z-Engine/src/Engine/Engine.h
+++ Z-Engine/src/Engine/Engine.h
@@ -1,5 +1,12 @@
 #pragma once
 #include <d3d11.h>
+#include "DirectXMath.h"
+
+struct Vertex
+{
+	DirectX::XMFLOAT3 position;
+	DirectX::XMFLOAT4 color;
+};
 
 class Engine
 {
@@ -9,8 +16,14 @@ public:
 	void RenderFrame();
 	void CleanD3D();
 private:
-	IDXGISwapChain* mySwapchain;           
-	ID3D11Device* myDev;                   
+	IDXGISwapChain* mySwapchain;
+	ID3D11Device* myDev;
 	ID3D11DeviceContext* myDevcon;
-	ID3D11RenderTargetView* myBackBuffer;
+	ID3D11RenderTargetView* myRenderTargetView;
+	ID3D11Buffer* myTriangleVertBuffer;
+	ID3D11VertexShader* myVS;
+	ID3D11PixelShader* myPS;
+	ID3D10Blob* myVS_Buffer;
+	ID3D10Blob* myPS_Buffer;
+	ID3D11InputLayout* myVertLayout;
 };
