bbc807950d6637bac108678dbcda0523879def27
zzWeeZz <42831999+zzWeeZz@users.noreply.github.com>
2022-04-14 09:15:03 +0200

    small changes

--- Source/Alpine/src/Engine/Graphics/ShaderClasses/ComputeShader.cpp
+++ Source/Alpine/src/Engine/Graphics/ShaderClasses/ComputeShader.cpp
@@ -6,11 +6,11 @@
 
 bool Alpine::ComputeShader::Initialize(const std::filesystem::path& filePath)
 {
-	auto hr = D3DReadFileToBlob(filePath.c_str(), myBuffer.GetAddressOf());
-	assert(SUCCEEDED(hr));
+	//auto hr = D3DReadFileToBlob(filePath.c_str(), myBuffer.GetAddressOf());
+	////assert(SUCCEEDED(hr));
 
-	hr = DX11::GetDevice()->CreateComputeShader(myBuffer.Get()->GetBufferPointer(), myBuffer.Get()->GetBufferSize(), NULL, myShader.GetAddressOf());
-	assert(SUCCEEDED(hr));
+	//hr = DX11::GetDevice()->CreateComputeShader(myBuffer.Get()->GetBufferPointer(), myBuffer.Get()->GetBufferSize(), NULL, myShader.GetAddressOf());
+	//assert(SUCCEEDED(hr));
 	return true;
 }
 
